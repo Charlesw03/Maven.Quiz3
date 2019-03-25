1ef63cd7e9ff576cde78aed15d2da3916911fe2a
Charles Wilmer <charleswilmer@zipcoders-MacBook-Pro.local>
2019-03-25 09:42:16 -0400

    made changes

--- src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
@@ -10,6 +10,9 @@ import java.util.stream.Stream;
  * @author leon on 09/12/2018.
  */
 public class SquareArrayAnalyzer {
+
+
+
     public static Boolean compare(Integer[] input, Integer[] squaredValues) {
         Boolean answer = false;
         for (int i = 0; i < input.length; i++) {
